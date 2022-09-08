4c04bb735e42740624ca7384b09d3950e378c4c0
ChristianoCastro <111785366+ChristianoCastro@users.noreply.github.com>
2022-09-08 18:06:13 -0400

    Add files via upload

---
+++ src/modelo/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package modelo.dao;
+
+import db.DB;
+import modelo.dao.impl.DepartmentDaoJDBC;
+import modelo.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+}
